baa58949b31faa70cd5b04533b5717370ace6b70
Habeeb Olufunke <Habeeb.O@bullseye.local>
2023-05-06 08:45:28 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('button');
+
+
+// function myfunction() {
+
+    // var element = document.body
+    btn.addEventListener('click', ()=>{
+    // element.classList.toggle ("dark-mode");
+      document.querySelector('body').classList.toggle('dark')
+    
+})
